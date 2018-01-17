4c79540f117ecce38cfbdbe221e9a23628219454
NAOSUKE <naosuke@somewhere.com>
2018-01-17 09:46:26 +0700

    First Commit : Crate Student Model

---
+++ Chapter09/src/model/Student.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Student {
+	private String id;
+	private String name;
+	private String branch;
+	private int age;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBranch() {
+		return branch;
+	}
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+}
